e4ecd398e4f3c78595feb70791b52fdb5babe968
Kurt Schneider <k.w.schneid@gmail.com>
2017-10-25 10:08:03 -0400

    Redux for selected book state

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
 	return [
-		{title: 'Javascript: the good parts'},
-		{title: 'Harry Potter'},
-		{title: 'Spooky Tower'},
-		{title: 'Michel soft'},
+		{title: 'Javascript: the good parts', pages:101},
+		{title: 'Harry Potter', pages:39},
+		{title: 'Spooky Tower', pages:83},
+		{title: 'Michel soft', pages:28},
 	]
 }
